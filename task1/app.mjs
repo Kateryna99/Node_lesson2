ebc5a5aeb4dea82d2a1faee55e06d8e5166f1076
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-10-13 19:19:07 +0200

    task1, improving solution

--- task1/app.mjs
+++ task1/app.mjs
@@ -6,30 +6,31 @@ const pensionAge = parseInt(new URLSearchParams(paramsString).get('--pension'));
 
 const rl = createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
-rl.question(
-    'How old are you?',
-    (age) => {
-        if (age < 0 || age > 120 || isNaN(age)){
+rl.setPrompt('How old are you?');
+rl.prompt();
+
+rl.on('line', (age) => {
+        if (age < 0 || age > 120) {
             console.log('Invalid age');
             rl.close();
             return;
         }
 
-
-            if (age < pensionAge)
-                console.log('You are not a pensioner')
-            else
-                console.log('You are a pensioner');
+        if (age < pensionAge)
+            console.log('You are not a pensioner')
+        else
+            console.log('You are a pensioner');
 
         rl.close();
     })
 
 rl.on('SIGINT', () => {
-    rl.question('Exit (y or n)?', (input) => {
-        if (input.match(/^y(es)?$/i))
-            rl.close();
-    })
-})
+    rl.question('Exit (y or n)? ', (input) => {
+        if (input.match(/^y(es)?$/i)) {
+            rl.pause();
+        }
+    });
+});
